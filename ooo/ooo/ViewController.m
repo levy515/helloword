74897dbadcb12b93435b76769b6cbc7836532fe1
liwei <laughing_li@greentown.cn>
2016-09-20 10:25:14 +0800

    sss

---
+++ ooo/ooo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ooo
+//
+//  Created by 李威 on 16/9/20.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"sss");
+}
+
+
+@end
